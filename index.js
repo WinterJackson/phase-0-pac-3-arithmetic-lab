910834324d6dd61e77c386fcbad6fe0b12b368b5
WinterJackson <winterjacksonwj@gmail.com>
2023-06-10 17:41:44 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(x,y){
+    let result =  x + y;
+    return result
+}
+function subtract(x,y){
+    let result = x - y;
+    return result
+}
+function multiply(x,y){
+    let result = x * y
+    return result
+}
+function divide(x,y){
+    let result = x / y
+    return result
+}
+function increment(n){
+    return n + 1;
+}
+function decrement(n){
+    return n -1;
+}
+function makeInt(n){
+    return  parseInt(n, 10);
+ }
+ function preserveDecimal(n){
+     return parseFloat(n)
+ }
